08d95b26ed08efb9b3d2cab864ef7fa2bb12998e
SebastienDiGiovanni <sebastien.di-giovanni@insa-lyon.fr>
2017-01-26 23:38:00 +0100

    Creation of the VisitedRoom Project.

---
+++ Assets/FirstPersonCamera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Camera.main != null)
+		{
+			if (Input.GetKey(KeyCode.UpArrow))
+			{
+				Camera.main.transform.Translate(new Vector3(0.0f, 0.0f, 0.1f));
+			}
+			if (Input.GetKey(KeyCode.DownArrow))
+			{
+				Camera.main.transform.Translate(new Vector3(0.0f, 0.0f, -0.1f));
+			}
+			if (Input.GetKey(KeyCode.RightArrow))
+			{
+				Camera.main.transform.Rotate(new Vector3(0.0f, 1.0f, 0.0f));
+			}
+			if (Input.GetKey(KeyCode.LeftArrow))
+			{
+				Camera.main.transform.Rotate(new Vector3(0.0f, -1.0f, 0.0f));
+			}
+		}
+	}
+}
