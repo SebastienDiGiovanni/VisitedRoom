1b74f80d55683801031d4c2f9e1d6fbea0885158
Ceyb <s.di-giovanni@hotmail.fr>
2017-01-29 16:18:41 +0100

    Window and light interaction added.

---
+++ Assets/LightInteraction.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class LightInteraction : MonoBehaviour {
+
+	private GameObject light;
+	private bool on;
+	private bool showOnLabel;
+	private bool showOffLabel;
+
+	// Use this for initialization
+	void Start () {
+		light = GameObject.Find("Light0");
+		on = true;
+		showOnLabel = false;
+		showOffLabel = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Camera.main != null)
+		{
+			if (light.GetComponent<Light>().intensity > 0)
+			{
+				on = true;
+			}
+			else
+			{
+				on = false;
+			}
+			
+			Vector3 cameraPosition = Camera.main.transform.position;
+			Vector3 lightPosition = light.transform.position;
+			double euclidianDistance = Math.Sqrt(Math.Pow(lightPosition.x - cameraPosition.x,2)+Math.Pow(lightPosition.y - cameraPosition.y,2)+Math.Pow(lightPosition.z - cameraPosition.z,2));
+			if (euclidianDistance < 0.8D)
+			{
+				if (on == false)
+				{
+					showOnLabel = true;
+					if (Input.GetKeyDown(KeyCode.L))
+					{
+						light.GetComponent<Light>().intensity = 1;
+						showOnLabel = false;
+					}
+				}
+				else
+				{
+					showOffLabel = true;
+					if (Input.GetKeyDown(KeyCode.L))
+					{
+						light.GetComponent<Light>().intensity = 0;
+						showOffLabel = false;
+					}
+				}
+			}
+			else
+			{
+				showOnLabel = false;
+				showOffLabel = false;
+			}
+		}
+	}
+	
+	void OnGUI() {
+		if (showOnLabel == true)
+		{
+			GUI.Label(new Rect(525, 400, 200, 20), "Press l to switch on the light.");
+		}
+		else if (showOffLabel == true)
+		{
+			GUI.Label(new Rect(525, 400, 200, 20), "Press l to switch off the light.");
+		}
+    }
+}
--- Assets/PianoInteraction.cs
+++ Assets/PianoInteraction.cs
@@ -6,38 +6,70 @@ using System;
 public class PianoInteraction : MonoBehaviour {
 
 	private GameObject piano;
-	private bool showPianoLabel;
+	private bool playingPiano;
+	private bool showStartPianoLabel;
+	private bool showStopPianoLabel;
 
 	// Use this for initialization
 	void Start () {
 		piano = GameObject.Find("piano");
-		showPianoLabel = false;
+		playingPiano = false;
+		showStartPianoLabel = false;
+		showStopPianoLabel = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Camera.main != null)
 		{
+			if (piano.GetComponent<AudioSource>().isPlaying)
+			{
+				playingPiano = true;
+			}
+			else
+			{
+				playingPiano = false;
+			}
+			
 			Vector3 cameraPosition = Camera.main.transform.position;
 			Vector3 pianoPosition = piano.transform.position;
 			double euclidianDistance = Math.Sqrt(Math.Pow(pianoPosition.x - cameraPosition.x,2)+Math.Pow(pianoPosition.y - cameraPosition.y,2)+Math.Pow(pianoPosition.z - cameraPosition.z,2));
-			if (euclidianDistance < 2D)
+			if (playingPiano == false)
 			{
-				showPianoLabel = true;
-				if (Input.GetKey(KeyCode.P))
+				if (euclidianDistance < 2D)
 				{
-					piano.GetComponent<AudioSource>().Play();
+					showStartPianoLabel = true;
+					if (Input.GetKeyDown(KeyCode.P) || playingPiano == true)
+					{
+						piano.GetComponent<AudioSource>().Play();
+						showStartPianoLabel = false;
+						showStopPianoLabel = true;
+					}
+				} 
+				else
+				{
+					showStartPianoLabel = false;
 				}
-			} else {
-				showPianoLabel = false;
+			}
+			else
+			{
+				if (Input.GetKeyDown(KeyCode.P))
+				{
+					piano.GetComponent<AudioSource>().Stop();
+					showStopPianoLabel = false;
+				}
 			}
 		}
 	}
 	
 	void OnGUI() {
-		if (showPianoLabel == true)
+		if (showStartPianoLabel == true)
 		{
-        GUI.Label(new Rect(550, 400, 150, 20), "Press p to play piano !");
+			GUI.Label(new Rect(550, 400, 150, 20), "Press p to play piano !");
+		}
+		else if (showStopPianoLabel == true)
+		{
+			GUI.Label(new Rect(525, 400, 200, 20), "Press p to stop playing piano.");
 		}
     }
 }
